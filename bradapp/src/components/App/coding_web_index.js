f588b60184a09f2f5f5c5f5b90100d0f50e2702d
BradHuang <adghbc01@tamu.edu>
2018-08-12 06:59:57 +0000

    Online Coding Web setup.

---
+++ bradapp/src/components/App/coding_web_index.js
@@ -0,0 +1,41 @@
+
+import React, {Component} from "react";
+import axios from "axios";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { questions: [] };
+    }
+    componentDidMount() {
+        axios({ method: "get", url: "http://api.haochuan.io/oj/problems" })
+            .then(response => {
+                console.log(response);
+                this.setState({ questions: response.data.questions });
+            })
+            .catch(err => {
+                console.log(err);
+                alert(err);
+            });
+    }
+    render() {
+        return (
+            <div>
+                <ul>
+                    { this.state.questions.map((item, index) => {
+                        return <li key={index}>{item.title}</li>;
+                    }) }
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+
+
+
+
+
