3fe9308ba3e77d39cf88ac5ebff58b7f07d7429a
BradHuang <adghbc01@tamu.edu>
2018-08-20 01:46:36 +0000

    combineReducers()

---
+++ redux-app/src/reducers/filter.test.js
@@ -0,0 +1,15 @@
+
+import filter from './filter';
+const expect = require("expect");
+
+describe('Test for filter reducer', () => {
+  it('Should return "all" for default state', () => {
+    expect(filter(undefined, {})).toEqual('all');
+  });
+  it('handle SET_FILTER action', () => {
+    const action = {type: 'SET_FILTER', filter: 'active'};
+    const result = 'active';
+    expect(filter(undefined, action)).toEqual(result);
+  });
+});
+
