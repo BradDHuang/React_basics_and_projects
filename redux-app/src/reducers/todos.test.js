e6318c33a89b57d3e297a71ef4ceab55fede18c3
BradHuang <adghbc01@tamu.edu>
2018-08-20 01:18:46 +0000

    upgrade node -v to support import & spread operator for Testing.

---
+++ redux-app/src/reducers/todos.test.js
@@ -0,0 +1,31 @@
+
+import todos from "./todos";
+
+const expect = require("expect");
+
+describe('Test for todos reducer', () => {
+  it('Should return empty array as init value', () => {
+    expect(todos(undefined, {})).toEqual([]);
+  });
+  it('handle ADD_TODO action', () => {
+    const action = {type: 'ADD_TODO', id: 1, text: 'first item'};
+    const result = [{id: 1, text: 'first item', completed: false}];
+    expect(todos(undefined, action)).toEqual(result);
+  });
+  it('handle TOGGLE action', () => {
+    const addAction = {type: 'ADD_TODO', id: 1, text: 'first item'};
+    const toggleAction = {type: 'TOGGLE_TODO', id: 1};
+    const result = [{id: 1, text: 'first item', completed: true}];
+    const newState = todos(undefined, addAction);
+    expect(todos(newState, toggleAction)).toEqual(result);
+  });
+});
+
+
+
+
+
+
+
+
+
