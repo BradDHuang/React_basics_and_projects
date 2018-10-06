a2a2a92239a9d870535f9a6bcde85455be0140c5
BradHuang <adghbc01@tamu.edu>
2018-10-06 19:49:38 +0000

    review: testing with jest.

--- redux-app/src/example.test.js
+++ redux-app/src/example.test.js
@@ -5,6 +5,10 @@ function sum(a, b) {
   return a + b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
 describe('test for sum function', () => {
   it('should return 3 when pass 1 and 2', () => {
     expect(sum(1, 2)).toEqual(3);
@@ -14,7 +18,14 @@ describe('test for sum function', () => {
   });
 });
 
-
+describe("test for multiply func.", () => {
+  it("should return 9 when pass 3 and 3", () => {
+    expect(multiply(3, 3)).toEqual(9);
+  });
+  it("should return 3 when pass 3 and 1", () => {
+    expect(multiply(3, 1)).toEqual(3);
+  });
+});
 
 
 
